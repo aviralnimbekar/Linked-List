8d548649e23cbeb96365a8f80272051321f2cd63
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-24 11:57:44 +0530

    ADDED : Welcome messege

---
+++ src/com/brigdelabz/MyLinkedList.java
@@ -0,0 +1,8 @@
+package com.brigdelabz;
+
+public class MyLinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked List Practice Problem");
+    }
+}
---
+++ src/com/brigdelabz/Utility.java
@@ -0,0 +1,4 @@
+package com.brigdelabz;
+
+public class Utility {
+}
