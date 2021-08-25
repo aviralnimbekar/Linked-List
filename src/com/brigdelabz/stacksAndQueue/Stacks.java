b369b9303ed83f297a5ede2259005d884326be23
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-25 23:40:51 +0530

    ADDED : Stack for given elements

--- src/com/brigdelabz/stacksAndQueue/Stacks.java
+++ src/com/brigdelabz/stacksAndQueue/Stacks.java
@@ -1,8 +1,16 @@
 package com.brigdelabz.stacksAndQueue;
 
+import com.brigdelabz.linkedList.MyNode;
+
 public class Stacks {
 
     public static void main(String[] args) {
         System.out.println("STACKS");
+
+        MyNode<Integer> myStack = new MyNode<>();
+        //add = push
+        myStack.add(70);
+        myStack.add(30);
+        myStack.add(56);
     }
 }
